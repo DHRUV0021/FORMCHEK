873087d2c5b960718f6b4e7138b8c160472b5c33
Dhruv Gorasiya <dhruv.girasiya@adrta.onmicrosoft.com>
2025-08-12 09:16:31 +0530

    Form Builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
+import { Formio, FormioModule, FormioRefreshValue } from '@formio/angular';
+import { FormioGrid } from '@formio/angular/grid';
+import { PrismService } from './Prism.service';
+
+// Make sure we use fontawesome everywhere in Form.io renderers.
+(Formio as any).icons = 'fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormioModule,
+    FormioGrid,],
+  providers: [PrismService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'FORMCHEK';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('json', { static: true }) jsonElement?: ElementRef;
+  @ViewChild('code', { static: true }) codeElement?: ElementRef;
+  public form: Object;
+  public refreshForm: EventEmitter<FormioRefreshValue> = new EventEmitter();
+  constructor(public prism: PrismService) {
+    this.form = { components: [] };
+  }
+
+  onChange(event: any) {
+    if (this.jsonElement && this.jsonElement.nativeElement) {
+      this.jsonElement.nativeElement.innerHTML = '';
+      this.jsonElement.nativeElement.appendChild(document.createTextNode(JSON.stringify(event.form, null, 4)));
+    }
+
+    this.refreshForm.emit({
+      property: 'form',
+      value: event.form
+    });
+    this.test(event.form)
+  }
+
+
+  ngAfterViewInit() {
+    this.prism.init();
+  }
+
+
+  test(form:any) {
+    console.log("WORK kar gya bhai", form)
+  }
 }
